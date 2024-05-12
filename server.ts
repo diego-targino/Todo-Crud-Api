4e3c58acdf4d8c3d3cc59bb8e5a1afde9a003207
diego-targino <diegotargino190@gmail.com>
2024-05-12 19:06:15 -0300

    feat: adição de politicas de cors

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import { userRouter } from "./src/routes/userRoutes";
 import { todoRouter } from "./src/routes/todoRoutes";
@@ -7,6 +8,12 @@ export const app: express.Application = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+  })
+);
+
 app.use("/users", userRouter);
 app.use("/todos", todoRouter);
 app.use("/categories", categoryRouter);
